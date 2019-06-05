fa345bea12d8ae78672a127226e85652098d431e
Måns Nilsson <mans.woxulv@gmail.com>
2019-06-05 23:22:53 +0200

    Add footer and add some padding

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
         <img src={me} className="me-logo" alt="månsnilsson" />
         <h1>Måns Nilsson</h1>
       </header>
+      <footer>
+        <a href="mailto:willaddthis@soon.com">email</a>•
+        <a href="http://twitter.com/the_mni">twitter</a>•
+        <a href="http://github.com/mansn">github</a>
+      </footer>
     </div>
   );
 }
