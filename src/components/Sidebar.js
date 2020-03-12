aa6518c3c0877347e8f6b9630003ba3d0f28fafe
mansn <4518977+mansn@users.noreply.github.com>
2020-03-12 22:37:12 +0100

    feat: add social media icons

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styled from '@emotion/styled'
+const Sidebar = () => {
+  const List = styled.ul`
+    background-color: green;
+  `
+
+  return (
+    <List>
+      <div>asdf</div>
+    </List>
+  )
+}
+
+export default Sidebar
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import './fonts/Montserrat/Montserrat-Light.ttf'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
-serviceWorker.unregister();
+serviceWorker.unregister()
