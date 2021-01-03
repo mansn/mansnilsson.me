59cf52971f594c2fba3cb861d2667cd9916e23e1
mansn <4518977+mansn@users.noreply.github.com>
2021-01-03 23:28:46 +0100

    refactor: eject create-react-app and install gatsby

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 import './fonts/Montserrat/Montserrat-Light.ttf'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-serviceWorker.unregister()
