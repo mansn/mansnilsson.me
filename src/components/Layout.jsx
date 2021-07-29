9df6021210894b6b837235a4eafcb937fc1901b2
Måns Nilsson <4518977+mansn@users.noreply.github.com>
2021-07-30 00:52:20 +0200

    feat: migrate to gatsby (#18)
    * refactor: eject create-react-app and install gatsby
    
    * refactor: remove public folder and add initial setup for gatsby
    
    * refactor: remove unused stuff
    
    * refactor: setup using minimal gatsby template and add initial content
    
    * feat: add more styling
    
    * feat: use logo as styled component
    
    * chore: bump lockfile due to lockVersion update
    
    * feat: start migrating to new structure with reusable components #wip
    
    * feat: update styling to go for the holy grail layout
    
    * feat: add font and further improve layout
    
    * fix: add fallback fonts
    
    * chore: tweak styling of grid of the me component
    
    * chore: add author and siteUrl to gatsby-config

---
+++ src/components/Layout.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import Footer from "../components/Footer";
+import "../css/typography.css";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    box-sizing: border-box;
+    margin: 0 auto;
+    background: #041122;
+    color: white;
+    padding: 96px;
+    font-family: MontSerrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    line-height: 1.7;
+  }
+
+  svg {
+    fill: hsl(0deg 0% 100%)
+  }
+
+  a:hover svg {
+    fill: rgba(255, 20, 147, 0.8);
+  }
+
+  @media (max-width: 767px) {
+    body {
+      padding: 20px;
+    }
+    
+    h1 {
+      font-size: 1.40em
+    }
+  }
+`;
+
+const StyledMain = styled.main`
+  display: grid;
+  grid-template-columns: 1fr min(65ch, 100%) 1fr;
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <StyledMain>
+      <GlobalStyle />
+      {children}
+      <Footer />
+    </StyledMain>
+  );
+};
+
+export default Layout;
---
+++ src/pages/index.jsx
@@ -0,0 +1,145 @@
+import * as React from "react";
+import { Layout } from "../components";
+import logo from "../images/me.webp";
+import logoFallback from "../images/me.jpg";
+import styled from "styled-components";
+
+const linkStyle = {
+  marginTop: "1em",
+  textDecoration: "none",
+  fontWeight: "bold",
+  color: "rgb(255, 20, 147)"
+};
+
+const Logo = ({ className }) => {
+  return (
+    <picture>
+      <source type="image/webp" srcSet={logo} />
+      <img src={logoFallback} className={className} alt="månsnilsson" />
+    </picture>
+  );
+};
+
+const StyledLogo = styled(Logo)`
+  grid-column: logo;
+  height: 30vmin;
+  pointer-events: none;
+  mask-image: radial-gradient(rgba(0, 0, 0, 1) 65%, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0) 100%);
+`;
+
+const StyledName = styled.div`
+  grid-column: name;
+  margin: auto;
+`;
+
+const Me = ({ className }) => {
+  return (
+    <div className={className}>
+      <StyledLogo />
+      <StyledName>
+        <h1>Måns Nilsson</h1>
+      </StyledName>
+    </div>
+  );
+};
+
+const StyledList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Portfolio = ({ className }) => {
+  return (
+    <div className={className}>
+      <h3>Portfolio</h3>
+      <StyledList className="portfolio">
+        <a style={linkStyle} href="https://github.com/weaddquality/business-model-canvas">
+          Business Model Canvas (React Web App)
+        </a>
+        <a style={linkStyle} href="https://github.com/weaddquality/bmc-aws-lambda-serverless">
+          Business Model Canvas (Serverless backend)
+        </a>
+        <a style={linkStyle} href="https://github.com/mansn/workout">
+          Workout App{" "}
+          <span role="img" aria-label="man weightlifter">
+            🏋️‍♂️ (React JAMStack App)
+          </span>
+        </a>
+      </StyledList>
+    </div>
+  );
+};
+
+const Intro = ({ className }) => {
+  return (
+    <div className={className}>
+      <p>
+        Hello there{" "}
+        <span role="img" aria-label="hand waving">
+          👋
+        </span>
+      </p>
+      <p>
+        I&apos;m a Fullstack Developer within the JavaScript stack. I work at{" "}
+        <a style={linkStyle} href="https://www.qred.com/">
+          Qred AB
+        </a>
+        .
+      </p>
+      <p>
+        This is my personal site where I share and process thoughts I have on my mind, mostly
+        related my experiences in software development, the world of work and my personal
+        development.
+      </p>
+      <p>
+        I also provide a list of personal projects at the bottom of the page for anyone that&apos;s
+        interested{" "}
+        <span role="img" aria-label="slightly smiling face">
+          🙂
+        </span>
+      </p>
+      <p>
+        I think the{" "}
+        <a
+          style={linkStyle}
+          href="https://www.angryweasel.com/ABTesting/modern-testing-principles/"
+        >
+          Modern Testing Principles
+        </a>{" "}
+        are amazing, and highly recommend everyone to embrace them.{" "}
+        <span role="img" aria-label="slightly smiling face">
+          🙂
+        </span>
+      </p>
+    </div>
+  );
+};
+
+const StyledIntro = styled(Intro)`
+  grid-column: 2;
+`;
+
+const StyledMe = styled(Me)`
+  display: grid;
+  grid-template-columns: [logo] 1fr [name] 1fr;
+  justify-content: center;
+  text-align: center;
+  grid-column: 2;
+`;
+
+const StyledPortfolio = styled(Portfolio)`
+  grid-column: 2;
+`;
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <title>mansnilsson.me</title>
+      <StyledMe />
+      <StyledIntro />
+      <StyledPortfolio />
+    </Layout>
+  );
+};
+
+export default IndexPage;
