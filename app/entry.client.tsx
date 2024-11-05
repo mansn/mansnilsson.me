ff194f7987aefc0a2426b049ac2f53ab97eb97cf
mansn <4518977+mansn@users.noreply.github.com>
2024-11-05 23:08:25 +0100

    feature: setup posthog

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,25 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from '@remix-run/react'
+import { startTransition, StrictMode, useEffect } from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import posthog from 'posthog-js'
 
-hydrateRoot(document, <RemixBrowser />);
+function PosthogInit() {
+  useEffect(() => {
+    posthog.init('phc_W09Kgul74R645XHwzslTTnrBeQ83hOEjaqJc44LEDJ9', {
+      api_host: 'https://eu.i.posthog.com',
+      person_profiles: 'always',
+    })
+  }, [])
+
+  return null
+}
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+      <PosthogInit />
+    </StrictMode>
+  )
+})
