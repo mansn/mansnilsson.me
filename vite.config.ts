b4876920e8cf411642e396dc8b32c20e070b31e8
mansn <4518977+mansn@users.noreply.github.com>
2025-07-06 23:45:33 +0200

    chore: complete upgrade by updating tsconfig, vite config and adding a react router config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,16 @@
 import mdx from '@mdx-js/rollup'
-import { vitePlugin as remix } from '@remix-run/dev'
+import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import remarkFrontmatter from 'remark-frontmatter'
 import wyw from '@wyw-in-js/vite'
-
-declare module '@remix-run/node' {
-  interface Future {
-    v3_singleFetch: true
-  }
-}
 
 export default defineConfig({
   plugins: [
     mdx({
       remarkPlugins: [remarkFrontmatter],
     }),
-    remix({
-      ignoredRouteFiles: ['**/*.css'],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: true,
-      },
-    }),
+    reactRouter(),
     tsconfigPaths(),
     wyw({
       include: ['**/*.{ts,tsx}'],
